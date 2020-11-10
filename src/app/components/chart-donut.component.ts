2bdc7395f48a66451a28ddbf44bdd23ae284d047
Jose Ramón Vieites Romay <fabelaretorcida@gmail.com>
2020-11-10 11:16:47 +0100

    Porcentage dinamico

--- src/app/components/chart-donut.component.ts
+++ src/app/components/chart-donut.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'chart-donut',
   templateUrl: 'chart-donut.component.html',
-  styleUrls: ['chart-donut.component.scss']
+  styleUrls: ['chart-donut.component.scss'],
 })
+export class ChartDonutComponent implements OnInit {
+  @Input() percentage;
 
-export class ChartDonutComponent {
-  @Input() percentage: number;
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.setProperty(
+      '--percentage',
+      this.percentage
+    );
+  }
 }
